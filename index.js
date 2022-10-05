d7b20089403ede395a2e956fc2fd10506bd571c5
hgferrer <hgferrer@gmail.com>
2022-10-05 12:14:14 -0300

    my initial server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+
+
+//Creamos el servidor
+const app = express();
+
+//Conectamoa a la  DB
+conectarDB();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'));
+
+
+
+
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo OK en puerto ${4000}`)
+})
